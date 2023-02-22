b234ce5cc71c8907573b4f9d54205c96f5e6060a
Joanna <astronautnemo@gmail.com>
2023-02-22 09:50:58 +0300

    completed bonus challenge

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,17 +28,21 @@ class Dog
     end
 
     def save
-        sql = <<-SQL
-            INSERT INTO dogs (name, breed)
-            VALUES (?, ?)
-        SQL
+        if self.id != nil
+            self.update
+        elsif self.id == nil
+            sql = <<-SQL
+                INSERT INTO dogs (name, breed)
+                VALUES (?, ?)
+            SQL
 
-        # Insert the dog
-        DB[:conn].execute(sql, self.name, self.breed)
+            # Insert the dog
+            DB[:conn].execute(sql, self.name, self.breed)
 
-        # Get the ID and save it to the Ruby instance @id
-        self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-
+            # Get the ID and save it to the Ruby instance @id
+            self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        end
+        
         self
     end
 
@@ -84,5 +88,22 @@ class Dog
         DB[:conn].execute(sql, id).map do |row|
             self.new_from_db(row)
         end.first
+    end
+
+    def self.find_or_create_by(name:, breed:)
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+
+        if !dog.empty?
+            dog_object = dog[0]
+            dog = self.new(id: dog_object[0], name: dog_object[1], breed: dog_object[2])
+        else
+            dog = self.create(name: name, breed: breed)
+        end
+        dog
+    end
+
+    def update
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
     end
 end
